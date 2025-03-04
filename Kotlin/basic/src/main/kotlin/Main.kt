caefecee34dce42d4388ec30d6d1d7ee0af87694
= <=>
2025-03-04 14:27:20 -0300

    up

--- Kotlin/basic/src/main/kotlin/Main.kt
+++ Kotlin/basic/src/main/kotlin/Main.kt
@@ -1,9 +1,16 @@
 package gersonaguiar
 
+// Simple function
 fun sayHello(name: String) {
     println("Hello, $name!")
 }
 
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
 fun main() {
     sayHello("Gerson")
+
+    print(sum(1,2))
 }
