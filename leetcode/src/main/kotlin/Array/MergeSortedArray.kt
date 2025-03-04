444f53319e1d4e1a0bdadd8f0e58c342da22e8e7
= <=>
2025-03-04 19:12:10 -0300

    leetcode Kotlin MergeSortedArray optimized.

--- leetcode/src/main/kotlin/Array/MergeSortedArray.kt
+++ leetcode/src/main/kotlin/Array/MergeSortedArray.kt
@@ -12,4 +12,28 @@ class MergeSortedArray {
         }
         nums1.sort()
     }
+
+    fun mergeOptimized(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var i = m - 1 // Pointer for nums1 (last valid element)
+        var j = n - 1 // Pointer for nums2 (last element)
+        var k = m + n - 1 // Pointer for final position in nums1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i]
+                i--
+            } else {
+                nums1[k] = nums2[j]
+                j--
+            }
+            k--
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j]
+            j--
+            k--
+        }
+
+    }
 }
