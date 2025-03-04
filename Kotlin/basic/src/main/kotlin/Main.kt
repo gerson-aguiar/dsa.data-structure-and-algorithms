0d1ff2cc497e793d810e07ade614a225e054663c
= <=>
2025-03-04 14:33:27 -0300

    up

--- Kotlin/basic/src/main/kotlin/Main.kt
+++ Kotlin/basic/src/main/kotlin/Main.kt
@@ -15,13 +15,41 @@ fun greet(name: String = "Guess") {
     println("Hello, $name!")
 }
 
+fun getStatus(code: Int) = when(code) {
+    200 -> "OK"
+    404 -> "Not found"
+    500 -> "Server Error"
+    else -> "Unknown"
+}
+
 fun main() {
     sayHello("Gerson")
 
-    print(sum(1,2))
+    println(sum(1,2))
 
-    print(multiply(2,3))
+    println(multiply(2,3))
 
     greet()
 
+    val age = 29
+    val canDrive = if(age>=18) "Can drive" else "Can't drive"
+    println(canDrive)
+
+    println(getStatus(404))
+
+    for (i in 1..5){
+        println(i)
+    }
+
+    var count = 3
+    while (count > 0){
+        println("Count: $count")
+        count--
+    }
+
+    val numbers = listOf(1,2,3,4,5)
+    println(numbers[2])
+
+    val evens = numbers.filter { it % 2 == 0 }
+    println(evens)
 }
